57c0bfbce0815207842a8f94a3358f3db429adf6
ALUMNADO\seruba <sergio_311096@hotmail.es>
2017-12-19 13:24:10 +0100

    subir reto

---
+++ PHP/reto.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #376;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    background-color: #438;
+}
+
+.active {
+    background-color: #4CBF53;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a href="#Portada">Portada</a></li>
+  <li><a href="#Quienes somos?">Quienes somos?</a></li>
+  <li><a href="#Contactanos">Contactanos</a></li>
+  <li style="float:right"><a class="active" href="#ajustes">Ajustes</a></li>
+</ul>
+
+</body>
+</html>
