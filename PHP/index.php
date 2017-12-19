08ee52717934eee4bf0e79a4b15c98877562c787
ALUMNADO\seruba <sergio_311096@hotmail.es>
2017-12-19 13:30:06 +0100

    subir php

---
+++ PHP/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aupa Levante</title>
+  </head>
+  <body>
+    <?php
+      //Definición de variables
+      $nombreEquipo="Levante";
+      $nombreEquipo2="Madrid";
+      $textoPantalla="Aupa" .$nombreEquipo;
+      $textoPantalla2="<br>A SEGUNDA " .$nombreEquipo2;
+      /*
+      Más definiciones
+      */
+      ?>
+      <h1><?=$textoPantalla?>
+      <br>
+      </h1>
+  </body>
+</html>
