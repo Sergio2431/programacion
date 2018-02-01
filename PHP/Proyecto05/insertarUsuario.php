f3b7a22954562431bf1c3fef5e28f2da28c4c51c
ALUMNADO\seruba <sergio_311096@hotmail.es>
2018-02-01 10:29:05 +0100

    subir proyecto 5

--- PHP/Proyecto05/insertarUsuario.php
+++ PHP/Proyecto05/insertarUsuario.php
@@ -3,46 +3,39 @@
   <head>
     <meta charset="utf-8">
     <title>Insertar Usuario</title>
+    <link rel="stylesheet" type="text/css" href="Formulario.css">
   </head>
   <body>
     <center>
-      <form action="proyecto5-2.php" method="post">
+      <form class="header" action="listadoUsuario.php" method="post">
         <div id="Registro">
           <h1>Registro de Usuarios</h1>
-          <tr>
-            <td>Nombre</td>
-            <td><input type="text" size="20" maxlength="80" name="Nombre" placeholder="Nombre del usuario"></td>
-            <br>
-          </tr>
-          <tr>
-            <br>
-            <td>Apellidos</td>
-            <td><input type="text" size="20" maxlength="80" name="Apellidos" placeholder="Apellidos del usuario"/></td>
-            <br>
-          </tr>
-          <tr>
-            <br>
-            <td>Edad</td>
-            <td><input type="text" size="20" maxlength="80" name="Edad" placeholder="Introduce la edad"></td>
-            <br>
-          </tr>
-          <tr>
-            <br>
-            <td>Curso</td>
-            <td><input type="text" size="20" maxlength="80" name="Curso" placeholder="Introduce el curso"></td>
-            <br>
-          </tr>
-          <tr>
-            <br>
-            <td>Puntuación</td>
-            <td><input type="text" size="20" maxlength="80" name="Puntuación" placeholder="Introduce la puntuación"></td>
-            <br>
-          </tr>
-          <br>
-            <form class="" action="listadoUsuario.php" method="post">
-              <input type="submit" value="Enviar este formulario" />
-            </form>
+          <div class="columna1">
+            <tr>
+              <td>Nombre</td>
+              <td><input type="text" size="20" maxlength="80" name="Nombre" placeholder="Nombre del usuario"></td><br>
+            </tr>
+            <tr>
+              <br><td>Apellidos</td>
+              <td><input type="text" size="20" maxlength="80" name="Apellidos" placeholder="Apellidos del usuario"/></td><br>
+            </tr>
+            <tr>
+              <br><td>Edad</td>
+              <td><input type="text" size="20" maxlength="80" name="Edad" placeholder="Introduce la edad"></td><br>
+            </tr>
+            <tr>
+              <br><td>Curso</td>
+              <td><input type="text" size="20" maxlength="80" name="Curso" placeholder="Introduce el curso"></td><br>
+            </tr>
+            <tr>
+              <br><td>Puntuación</td>
+              <td><input type="text" size="20" maxlength="80" name="Puntuación" placeholder="Introduce la puntuación"></td><br>
+            </tr>
+          </div>
+            <br><button type="submit">Registrarse</button>
+            <button type="reset">Restablecer</button>
         </div>
       </form>
+    </center>
   </body>
 </html>
