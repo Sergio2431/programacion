81a8c119aa540176fd9ca3a2d6b2ed1c5dece8ee
ALUMNADO\seruba <sergio_311096@hotmail.es>
2018-02-06 13:21:07 +0100

    subir proyecto 5

--- PHP/Proyecto05/listadoUsuario.php
+++ PHP/Proyecto05/listadoUsuario.php
@@ -3,8 +3,31 @@
   <head>
     <meta charset="utf-8">
     <title>Listado Usuarios</title>
+    <link rel="stylesheet" href="text/css/"href="formulariolistado.css">
   </head>
   <body>
-   
+    <?php
+      $conector = new mysqli("localhost","root","","programacion");
+      if ($conector->connect_errno) {
+        echo "Fallo al conectar a MySQL: " . $conector->connect_error;
+      }else {
+        $consulta=$conector->query('SELECT * FROM usuario');
+        ?>
+        <ol id="lista3">
+          <?php
+          foreach ($consulta as $fila) {
+          ?>
+            <li>
+              <?php
+                echo "Usuario: ". $fila["nombre"]." ". $fila["apellidos"]." ".$fila["edad"]."años ".$fila["curso"]."<br>";
+               ?>
+            </li>
+            <?php
+            }
+            ?>
+        </ol>
+        <?php
+        }
+       ?>
   </body>
 </html>
