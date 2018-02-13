c86f3ccc58cd0aece1d4ce6a7f43c8f2f4e08068
Sergio2431 <32122806+Sergio2431@users.noreply.github.com>
2018-02-13 13:41:39 +0100

    subir archivos

--- Ahorcado4/Ahorcado4.js
+++ Ahorcado4/Ahorcado4.js
@@ -11,22 +11,52 @@ console.log(numAle);
 var acertar=palabras[numAle];
 console.log(acertar);
 
-var barrabaja=8;
-var comp=false;
-var fallo="";
-var entrada=letra;
-
-//contador de fallos
-var intentos=5;
-var cont=0;
-if (intentos>cont && barrabaja!=0) {
+function Comprobar(){
+  if(intentos>cont && barrabaja!=0){
   letra=entrada.value;
   entrada.value="";
+
+  while (isNaN(letra)==false) {
+    letra=prompt("Introduce una letra");
+    alert("Error, introduce una letra")
+  }
+  }
 }
 
-//comprobar si la letra introducida esta en la palabra
-function comprobar(){
-  if(intentos>cont && barrabaja!=0)
-  //pedir letra y comprobamos si esta
-  
+//poner letra en la plantilla
+for (var i = 0; i <palabras.length-1; i++) {
+  if (letra == palabraAcertar[i]) {
+    palabras[i]=letra;
+    comp=true;
+  }
+}
+
+//comprobar si la letra esta en la palabra
+if (comp==true) {
+  console.log(letra);
+}else {
+  fallo=fallo+letra;
+  fallos.textContent=fallo;
+  cont++;
+
+  intentado.tectContent="Llevas "+cont+" intentos.";
+}
+comp=false;
+
+//array
+var j="";
+for (let i=0; i<=adivinar.length-1; i++){
+  j=j+palabras[i];
+}
+comprobante.value=j;
+
+//comprobar la letra introducida
+barrabaja=0;
+preguntsr=false;
+for(let i=0; i<=adivinar.length-1; i++){
+  if(palabra[i]=="_") barrabaja=barrabaja+1;
+  }else {
+  if (barrabaja==0) resultado.textContent="ENHORABUENA!!!";
+  else resultado.textContent="Mejor suerte la próxima vez";
+ }
 }
---
+++ PHP/Reto-PaisesMundo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Reto-Paises del Mundo</title>
+  </head>
+  <body>
+    <?php
+    
+     ?>
+  </body>
+</html>
