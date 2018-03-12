a6229fe13d9fcebb12e435255887c820f4044477
ALUMNADO\seruba <sergio_311096@hotmail.es>
2018-03-12 12:41:18 +0100

    subir archivos

--- PokemonDaw/index.php
+++ PokemonDaw/index.php
@@ -1,32 +1,48 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Titulo Pokemon</title>
+  </head>
+  <body>
+    <center>
+    <img src="pokemon.png" width="300" height="100"><br>
+    <?php
 
-require "./lib/Pokemon/RegiMachop.php";
-require "./lib/Pokemon/JosepMetapod.php";
-require "./lib/Pokemon/PabloOnix.php";
-require "./lib/pokemon/SergioMewtwo.php";
+    require "./lib/Pokemon/RegiMachop.php";
+    require "./lib/Pokemon/JosepMetapod.php";
+    require "./lib/Pokemon/PabloOnix.php";
+    require "./lib/pokemon/SergioMewtwo.php";
 
-$RegiMachop= new RegiMachop();
-echo "Caracteristicas de poder de RegiMachop:<br>";
-echo "RegiMachop tiene ".$RegiMachop-> getVida()." puntos de vida.<br>";
-echo "Su primer ataque Queja hace ".$RegiMachop-> getAtaqueQueja()." puntos de daño.<br>";
-echo "Su segundo ataque Furia hace ".$RegiMachop-> getAtaqueFuria()." puntos de daño.<br><br>";
+    echo '<h3>POKEMON REGIMACHOP</h3>';
+    $RegiMachop= new RegiMachop();
+    echo "Caracteristicas de poder de RegiMachop:<br>";
+    echo "RegiMachop tiene ".$RegiMachop-> getVida()." puntos de vida.<br>";
+    echo "Su primer ataque Queja hace ".$RegiMachop-> getAtaqueQueja()." puntos de daño.<br>";
+    echo "Su segundo ataque Furia hace ".$RegiMachop-> getAtaqueFuria()." puntos de daño.<br><br>";
 
-$JosepMetapod= new JosepMetapod();
-echo "Caracteristicas de poder de JosepMetapod:<br>";
-echo "JosepMetapod tiene ".$JosepMetapod->getVida()." puntos de vida.<br>";
-echo "Su primer ataque Chupa aumenta en  ".$JosepMetapod-> getDefensaChupa()." sus puntos de vida.<br>";
-echo "Su segundo ataque Jala hace ".$JosepMetapod-> getAtaqueJala()." puntos de daño.<br><br>";
+    echo '<h3>POKEMON JOSEPMETAPOD</h3>';
+    $JosepMetapod= new JosepMetapod();
+    echo "Caracteristicas de poder de JosepMetapod:<br>";
+    echo "JosepMetapod tiene ".$JosepMetapod->getVida()." puntos de vida.<br>";
+    echo "Su primer ataque Chupa aumenta en  ".$JosepMetapod-> getDefensaChupa()." sus puntos de vida.<br>";
+    echo "Su segundo ataque Jala hace ".$JosepMetapod-> getAtaqueJala()." puntos de daño.<br><br>";
 
-$PabloOnix= new PabloOnix();
-echo "Caracteristicas de poder de PabloOnix:<br>";
-echo "PabloOnix tiene ".$PabloOnix-> getVida()." puntos de vida.<br>";
-echo "Su primer ataque Programador hace ".$PabloOnix-> getAtaqueProgramador()." puntos de daño.<br>";
-echo "Su segundo ataque Repositorio hace ".$PabloOnix-> getAtaqueRepositorio()." puntos de daño.<br><br>";
+    echo '<h3>POKEMON PABLOONIX</h3>';
+    $PabloOnix= new PabloOnix();
+    echo "Caracteristicas de poder de PabloOnix:<br>";
+    echo "PabloOnix tiene ".$PabloOnix-> getVida()." puntos de vida.<br>";
+    echo "Su primer ataque Programador hace ".$PabloOnix-> getAtaqueProgramador()." puntos de daño.<br>";
+    echo "Su segundo ataque Repositorio hace ".$PabloOnix-> getAtaqueRepositorio()." puntos de daño.<br><br>";
 
-$SergioMewtwo= new SergioMewtwo();
-echo "Caracteristicas de poder de SergioMewtwo:<br>";
-echo "PabloOnix tiene ".$SergioMewtwo-> getVida()." puntos de vida.<br>";
-echo "Su primer ataque Muleta hace ".$SergioMewtwo-> getAtaqueMuleta()." puntos de daño.<br>";
-echo "Su segundo ataque PiernaLoca hace ".$SergioMewtwo-> getAtaquePiernaLoca()." puntos de daño.<br>";
+    echo '<h3>POKEMON SERGIOMEWTWO</h3>';
+    $SergioMewtwo= new SergioMewtwo();
+    echo "Caracteristicas de poder de SergioMewtwo:<br>";
+    echo "SergioMewtwo tiene ".$SergioMewtwo-> getVida()." puntos de vida.<br>";
+    echo "Su primer ataque Muleta hace ".$SergioMewtwo-> getAtaqueMuleta()." puntos de daño.<br>";
+    echo "Su segundo ataque PiernaLoca hace ".$SergioMewtwo-> getAtaquePiernaLoca()." puntos de daño.<br>";
 
-?>
+    ?>
+  </center>
+  </body>
+</html>
