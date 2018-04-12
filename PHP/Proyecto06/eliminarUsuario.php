708926de37d15aee9ebb71ee7ac25d5fbad97e47
ALUMNADO\seruba <sergio_311096@hotmail.es>
2018-04-12 13:24:22 +0200

    subir proyecto 06

--- PHP/Proyecto06/eliminarUsuario.php
+++ PHP/Proyecto06/eliminarUsuario.php
@@ -20,26 +20,7 @@
         <div id="Registro">
           <h1>Eliminar Usuarios</h1>
           <div class="columna1">
-            <tr>
-              <td>Nombre</td>
-              <td><input type="text" size="20" maxlength="80" name="Nombre" placeholder="Nombre del usuario"></td><br>
-            </tr>
-            <tr>
-              <br><td>Apellidos</td>
-              <td><input type="text" size="20" maxlength="80" name="Apellidos" placeholder="Apellidos del usuario"/></td><br>
-            </tr>
-            <tr>
-              <br><td>Edad</td>
-              <td><input type="text" size="20" maxlength="80" name="Edad" placeholder="Introduce la edad"></td><br>
-            </tr>
-            <tr>
-              <br><td>Curso</td>
-              <td><input type="text" size="20" maxlength="80" name="Curso" placeholder="Introduce el curso"></td><br>
-            </tr>
-            <tr>
-              <br><td>Puntuación</td>
-              <td><input type="text" size="20" maxlength="80" name="Puntuación" placeholder="Introduce la puntuación"></td><br>
-            </tr>
+            
           </div>
             <br><button type="submit">Eliminar</button>
             <button type="reset">Restablecer</button>
