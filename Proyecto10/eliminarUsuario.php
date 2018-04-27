a8804af8afff230e7c881820e1c4dc021e766d25
ALUMNADO\seruba <sergio_311096@hotmail.es>
2018-04-27 11:48:24 +0200

    subir proyecto10

---
+++ Proyecto10/eliminarUsuario.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Eliminar Usuario</title>
+    <link rel="stylesheet" type="text/css" href="Formulario.css">
+  </head>
+  <body>
+    <?php
+      $pantalla="Lista";
+    ?>
+    <ul id="barra">
+      <li><a class="<?php if ($pantalla=="Crear") echo "active";?>" href="insertarUsuario.php">Crear Usuario</a></li>
+      <li><a class="<?php if ($pantalla=="Actualizar") echo "active";?>" href="actualizarUsuario.php">Actualizar Usuario</a></li>
+      <li><a class="<?php if ($pantalla=="Eliminar") echo "active";?>" href="eliminarUsuario.php">Eliminar Usuario</a></li>
+      <li><a class="<?php if ($pantalla=="Lista") echo "active";?>" href="listadoUsuario.php">Lista de Usuarios</a></li>
+    </ul>
+    <center>
+      <form class="header" action="eliminarUsuario1.php" method="post">
+        <div id="Registro">
+          <h1>Eliminar Usuarios</h1>
+          <div class="columna1">
+            <table>
+            <tr>
+              <td><b>Usuario</b></td>
+              <td>
+                <select id="seleccionar_usuario" name="seleccionar_usuario">
+                  <?php
+                    $conector = new mysqli("localhost","root","","programacion1");
+                    if ($conector->connect_errno){
+                      echo "Fallo al conectar a MySQL: " . $conector->connect_error;
+                    }else {
+                      $consulta=$conector->query('SELECT DNI FROM usuario');
+                      ?>
+                      <ol id="lista2" name="UsuarioEliminar">
+                        <?php
+                        foreach ($consulta as $fila) {
+                        ?>
+                          <option>
+                            <?php
+                              echo $fila["DNI"]."<br>";
+                            ?>
+                          </option>
+                            <?php
+                            }
+                            ?>
+                      </ol>
+                      <?php
+                      }
+                      ?>
+                  </select>
+                </td>
+              </tr>
+            </table>
+          </div>
+            <br><button type="submit">Eliminar</button>
+            <button type="reset">Restablecer</button>
+        </div>
+      </form>
+    </center>
+  </body>
+</html>
---
+++ Proyecto10/registro.php
@@ -0,0 +1,23 @@
+<?php
+//codigo php que conecta con la base de datos
+  $conector = new mysqli("localhost","root","","programacion1");
+  if ($conector->connect_errno){
+    echo "Fallo al conectar a MySQL: " . $conector->connect_error;
+  }else {
+    //almacenar en variables los datos recogidos
+    $nombre=$_POST['Nombre'];
+    $apellidos=$_POST['Apellidos'];
+    $DNI=$_POST['DNI'];
+    $edad=$_POST['Edad'];
+    $curso=$_POST['Curso'];
+    $puntuacion=$_POST['Puntuacion'];
+    //comprobar que todos los campos se han llenado
+    $llenado=(strlen($nombre)*strlen($apellidos)*strlen($DNI)*strlen($edad)*strlen($curso)*strlen($puntuacion)) or die("<center>
+    <h3>No se han llenado todos los campos <br> <a href='insertarUsuario.php'>Volver a la página de registro</a></h3>
+    </center>");
+    //añadir la info a la base de datos
+    $conector->query("INSERT INTO usuario VALUES ('$nombre','$apellidos','$edad','$curso','$puntuacion')") or die("<h3>Error de envio</h3>");
+    echo '<h2>Registro completo</h2>';
+  }
+?>
+<script type="text/javascript" id="identificador_js" src="comprobacion.js">
